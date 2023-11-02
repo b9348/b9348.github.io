2c5336b6ce2de774bb2fe33d7a45ac5490b2ad5a
b9348 <boris9348@qq.com>
2023-11-02 15:51:28 +0800

    tongyi lingma test

--- src/components/highlight.js
+++ src/components/highlight.js
@@ -12,15 +12,16 @@ function Highlight({ children, color, colorName }, ref) {
   if (colorName) {
     color = COLOR[colorName]
   }
+  // 使用useImperativeHandle钩子函数，当组件销毁时将ref对象传递给父组件，以供父组件使用
   useImperativeHandle(
-    ref,
+    ref, // ref对象用于保存对组件的引用
     () => {
       return {
-        colorName,
-        color,
+        colorName, // 颜色名
+        color, // 颜色值
       }
-    },
-    [color, colorName],
+    }, // 返回一个对象，包含颜色名和颜色值
+    [color, colorName], // 当color和colorName发生变化时，重新计算并返回新的对象
   )
 
   return (
